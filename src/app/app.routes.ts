4c05bf545f8b9b97f9efcc0218096551b8547eca
Kashiwa <liao2000@livemail.tw>
2024-03-16 21:57:13 +0800

    新增 page-not-found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { EventsComponent } from './events/events.component';
 import { HomeComponent } from './home/home.component';
 import { LiveComponent } from './live/live.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: 'events', component: EventsComponent },
     { path: 'live', component: LiveComponent },
     { path: '', component: HomeComponent },
+    { path: '**', pathMatch: 'full', component: PageNotFoundComponent }
 ];
